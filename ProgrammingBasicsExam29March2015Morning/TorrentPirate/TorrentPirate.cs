c21091abdde71d516468b57b7a1235b53a553385
IskraNikolova <air_iskra@yahoo.com>
2016-01-16 14:04:13 +0200

    add new solution

---
+++ ProgrammingBasicsExam29March2015Morning/TorrentPirate/TorrentPirate.cs
@@ -0,0 +1,33 @@
+﻿namespace TorrentPirate
+{
+    using  System;
+
+    public class TorrentPirate
+    {
+        public static void Main()
+        {
+            const int sizeOfMoviesInMB = 1500;
+            const int mBOnSecond = 2;
+            const int seconds = 60;
+            const int minutes = 60;
+
+            double megabytesDownwoad = double.Parse(Console.ReadLine());
+            double priceForTicket = double.Parse(Console.ReadLine());
+            double wifeSpending = double.Parse(Console.ReadLine());
+
+            double downLoadTimeInOursInThaMall = megabytesDownwoad/mBOnSecond/seconds/minutes;
+            double downloadPrice = downLoadTimeInOursInThaMall*wifeSpending;
+            double numberOfMovies = megabytesDownwoad/sizeOfMoviesInMB;
+            double cinemaPrice = numberOfMovies*priceForTicket;
+
+            if (downloadPrice <= cinemaPrice)
+            {
+                Console.WriteLine($"mall -> {downloadPrice:F2}lv");
+            }
+            else
+            {
+                Console.WriteLine($"cinema -> {cinemaPrice:F2}lv");
+            }
+        }
+    }
+}
